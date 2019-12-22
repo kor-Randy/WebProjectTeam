3e17555f372be418ac164c32a6c4620061cf8ac7
haebeompark <beom0528@kau.kr>
2019-12-22 20:40:06 +0900

    Update upload.php

--- project/upload.php
+++ project/upload.php
@@ -1,11 +1,11 @@
 <?php
- $productName = $_POST['pdname'];
+ $productName = $_COOKIE["pdid"];
  foreach ($_FILES['upload']['name'] as $f => $name) {   
     $uploadFile = $_FILES['upload']['name'][$f];
     $fileinfo = pathinfo($uploadFile);
     $ext = $fileinfo['extension'];   #확장자명 구하기
 
-    $imgSrc = "./images/" . $productName ."_". $f . "." . $ext;
+    $imgSrc = "./images/" . $productName ."_". $f . ".jpg";
     if(move_uploaded_file($_FILES['upload']['tmp_name'][$f], $imgSrc)){
         
     }else{
